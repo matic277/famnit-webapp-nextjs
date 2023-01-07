a898e744a4155d5a836235645b370130eea0c640
matic <matic277@gmail.com>
2023-01-07 16:23:35 +0100

    new api for user id (unsed, maybe unnecessary), fixing mynotes subpage, fontawesome dependency, minor css improvements, BREAKING: deleting notes for anons in home page

--- pages/mynotes/index.js
+++ pages/mynotes/index.js
@@ -2,22 +2,54 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { UserProvider, useUser } from '@auth0/nextjs-auth0/client';
 //import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 import NotesList from "../../components/NotesList";
 
 
-export default function Mynotes({ notesList }) {
-//export default withPageAuthRequired(function Profile({ user }) {
-    // Auth0 user state
-    //const { user, isLoading } = useUser();
-
-    // Standard react stuff to change state on client and server
-    //const [notes, setNotes] = useState(notesList);
+export default function Mynotes() {
+// export default withPageAuthRequired(function Profile({ user }) {
     
-    console.log("notes: ", notesList);
+    // Auth0 user state
+    const { user, isLoading } = useUser();
+    if (!user) {
+        return (<div>No user logged in</div>);
+    }
+    
+    // Grab users notes
+    const [notes, setNotes] = useState(null);
+    useEffect(() => {
+        console.log("Loading for user=", user.email);
+        // setNotesLoading(true);
+        fetch("/api/notes/user?name=" + user.email)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log("Fetched notes=", data);
+                data.forEach(n => {
+                    n.username = n.username == undefined ? "Anonymouse" : n.username;
+                });
+                setNotes(data);
+                // setNotesLoading(false);
+            });
+    }, []);
+    if (!notes) {
+        return (<div>No notes</div>);
+    }
+
+    // useEffect(() => {
+    //     console.log("Loading for userId=", userId);
+    //     setNotesLoading(true);
+    //     fetch("/api/notes/user?id=" + userId)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log("Fetched notes=", data);
+    //             setNotes(data);
+    //             setNotesLoading(false);
+    //         });
+    // }, []);
+    // if (!notes) return (<div>No notes</div>);
 
     // const fetchNotesForUser = async () => {
     //     const res = await fetch(
@@ -46,23 +78,16 @@ export default function Mynotes({ notesList }) {
         <>
             <div className={styles.contentContainer}>
                 <div className={styles.content}>
-                    {
-                        notesList ?
-                        <NotesList notes={notesList}/>
-                        :
-                        <div>loading</div>
-                    }
+                    <NotesList notes={notes}/>
                 </div>
             </div>
         </>
     );
 }
 
-Mynotes.getInitialProps = async (ctx) => {
-    const res = await fetch(
-        (process.env.environment == 'production' ? "https://famnit-webapp-nextjs.vercel.app" : "http://localhost:3000") +
-        '/api/notes/user?user_email=2'); // TODO set actual id
-    const notesList = await res.json();
-    console.log("props: ", notesList);
-    return { notesList };
-}
+// Mynotes.getInitialProps = async (ctx) => {
+//     const res = await fetch('/api/notes/user?id'); // TODO set actual id
+//     const notesList = await res.json();
+//     console.log("props: ", notesList);
+//     return { notesList };
+// }
