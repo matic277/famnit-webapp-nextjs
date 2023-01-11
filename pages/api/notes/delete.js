6d2cbbeb01a8886fc1e92ef8806f44e6ccc8e2ba
matic <matic277@gmail.com>
2023-01-11 19:14:35 +0100

    correct responses on note delete

--- pages/api/notes/delete.js
+++ pages/api/notes/delete.js
@@ -3,18 +3,24 @@ import conn from '../../../lib/db'
 export default function handler(req, res) {
     console.log("--------------------API request invoked (delete)------------------------");
     if (req.method !== 'DELETE') {
-        res.status(405).send({ message: 'Request method ' + req.method + ' used, but only DELETE requests are allowed!' })
-        return
+        res.status(405).send({ message: 'Request method ' + req.method + ' used, but only DELETE requests are allowed!' });
+        return;
     }
     try {
         const noteId = req.body.id_note;
         console.log("Trying to delete a note by id=", noteId);
-        if (!noteId) throw new Error("Deleting a note but id_note is not set:", noteId);
-
+        if (!noteId) {
+            const errMsg = "Deleting a note but id_note is not set: " + noteId;
+            console.error(errMsg);
+            res.status(405).send({ message: errMsg })
+            return;
+        }
         const query = 'DELETE FROM NOTE WHERE id_note=$1';
         const result = conn.query(query, [noteId]);
-        result.then(r => { res.status(200); });
+        result.then(r => { res.status(204); });
   } catch (err) {
+    const errMsg = 
+      res.status(495);
       console.log("Error occured:", err);
   }
 }
